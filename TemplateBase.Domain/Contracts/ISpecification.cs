1d1f57d8b467b24745d1d98f6eb1eaf44906b8fc
Felipe Libardi <flibardig@gmail.com>
2022-09-08 18:33:14 -0300

    Propriedades do specification setadas como nullable na interface.

--- TemplateBase.Domain/Contracts/ISpecification.cs
+++ TemplateBase.Domain/Contracts/ISpecification.cs
@@ -6,9 +6,9 @@ namespace TemplateBase.Domain.Contracts
 {
     public interface ISpecification<TEntity>
     {
-        Expression<Func<TEntity, bool>> Criteria { get; }
-        List<Expression<Func<TEntity, object>>> Includes { get; }
-        Expression<Func<TEntity, object>> OrderByAscend { get; }
-        Expression<Func<TEntity, object>> OrderByDescend { get; }
+        Expression<Func<TEntity, bool>>? Criteria { get; }
+        List<Expression<Func<TEntity, object>>>? Includes { get; }
+        Expression<Func<TEntity, object>>? OrderByAscend { get; }
+        Expression<Func<TEntity, object>>? OrderByDescend { get; }
     }
 }
