8119a3a5c07e513feecf920de6d037c0bf7c2537
Felipe Libardi <flibardig@gmail.com>
2022-10-01 15:06:57 -0300

    Feature: OperationCanceled middleware adicionado.

---
+++ TemplateBase.Domain/Middlewares/OperationCanceledMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace TemplateBase.Domain.Middlewares
+{
+    public class OperationCanceledMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const int _clientClosedRequest = 499;
+
+        public OperationCanceledMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context);
+
+            try
+            {
+                await _next(context).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+                context.Response.StatusCode = _clientClosedRequest;
+            }
+        }
+    }
+}
