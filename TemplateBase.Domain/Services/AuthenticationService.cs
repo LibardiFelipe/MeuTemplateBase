7e58e35430e18196893ab92b17372d961fb59ff1
Felipe Libardi <flibardig@gmail.com>
2022-10-05 21:42:12 -0300

    Feature: Rota de autenticação adicionada.

--- TemplateBase.Domain/Services/AuthenticationService.cs
+++ TemplateBase.Domain/Services/AuthenticationService.cs
@@ -1,6 +1,12 @@
 ﻿using Flunt.Notifications;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TemplateBase.Domain.Contracts;
@@ -15,10 +21,12 @@ namespace TemplateBase.Domain.Services
     public class AuthenticationService : Notifiable<Notification>, IAuthenticationService
     {
         private readonly IUnitOfWork _uow;
+        private readonly IConfiguration _configuration;
 
-        public AuthenticationService(IUnitOfWork uow)
+        public AuthenticationService(IUnitOfWork uow, IConfiguration configuration)
         {
             _uow = uow;
+            _configuration = configuration;
         }
 
         public async Task<AuthData> AuthenticateAsync(string email, string password, CancellationToken cancellationToken)
@@ -36,9 +44,29 @@ namespace TemplateBase.Domain.Services
                 Name = user.Name,
                 Email = user.Email,
                 Permission = user.Permission,
-                Token = ""
-            };
+                Token = GenerateToken(user)
+        };
         }
+
+        private string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("TokenSecret").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Permission.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
 
         public IReadOnlyCollection<Notification> GetNotifications() => Notifications;
         public bool IsInvalid() => Notifications.Any();
--- TemplateBase.WebAPI/Program.cs
+++ TemplateBase.WebAPI/Program.cs
@@ -1,11 +1,14 @@
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
 using TemplateBase.Application.Commands.Base;
 using TemplateBase.Application.Queries;
 using TemplateBase.Application.Queries.Base;
@@ -26,6 +29,26 @@ builder.Services.AddRouting(options =>
 
 // Controllers
 builder.Services.AddControllers();
+
+// Authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("TokenSecret").Value);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 // DataContext
 builder.Services.AddDbContext<DataContext>(x
@@ -94,6 +117,7 @@ app.UseMiddleware<OperationCanceledMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
